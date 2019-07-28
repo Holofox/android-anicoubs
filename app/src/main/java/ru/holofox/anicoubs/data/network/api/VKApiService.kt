45b04b79ea085648b5af89d472bb42debde4c7da
Holofox <holofox66@gmail.com>
2019-07-28 19:28:24 +0500

    Refactor VK API Methods

--- app/src/main/java/ru/holofox/anicoubs/data/network/api/VKApiService.kt
+++ app/src/main/java/ru/holofox/anicoubs/data/network/api/VKApiService.kt
@@ -1,16 +1,5 @@
 package ru.holofox.anicoubs.data.network.api
 
-import com.vk.api.sdk.VKApiManager
-import com.vk.api.sdk.VKApiResponseParser
-import com.vk.api.sdk.VKMethodCall
-import com.vk.api.sdk.exceptions.VKApiIllegalResponseException
-import com.vk.api.sdk.internal.ApiCommand
-
-import com.google.gson.GsonBuilder
-
-import org.json.JSONException
-import org.json.JSONObject
-
 import ru.holofox.anicoubs.data.network.response.VKWallGetResponse
 import ru.holofox.anicoubs.data.db.entity.vk.builder.VKParameters
 import ru.holofox.anicoubs.data.network.response.VKVideoSaveResponse
@@ -18,87 +7,31 @@ import ru.holofox.anicoubs.data.network.response.VKWallGetByIdResponse
 
 class VKApiService {
 
-    class WallGet(private val parameters: VKParameters): ApiCommand<VKWallGetResponse>() {
-
-        override fun onExecute(manager: VKApiManager): VKWallGetResponse {
-            val call = VKMethodCall.Builder()
-                .method("wall.get")
-                .args(parameters.args)
-                .version(API_VERSION) // manager.config.version
-                .build()
-
-            return manager.execute(call, ResponseApiParser())
-        }
-
-        private class ResponseApiParser : VKApiResponseParser<VKWallGetResponse> {
-            override fun parse(response: String): VKWallGetResponse {
-                try {
-                    val joResponse = JSONObject(response).getJSONObject("response")
-                    val gson = GsonBuilder().setPrettyPrinting().create()
-
-                    return gson.fromJson(joResponse.toString(), VKWallGetResponse::class.java)
-                } catch (ex: JSONException) {
-                    throw VKApiIllegalResponseException(ex)
-                }
-            }
-        }
+    companion object METHODS {
+        private const val WALL_GET = "wall.get"
+        private const val WALL_GET_BY_ID = "wall.getById"
+        private const val VIDEO_SAVE = "video.save"
     }
 
-    class VideoGetById(private val parameters: VKParameters) : ApiCommand<VKWallGetByIdResponse>() {
-
-        override fun onExecute(manager: VKApiManager): VKWallGetByIdResponse {
-            val call = VKMethodCall.Builder()
-                .method("wall.getById")
-                .args(parameters.args)
-                .version(API_VERSION)
-                .build()
-
-            return manager.execute(call, ResponseApiParser())
-        }
-
-        private class ResponseApiParser : VKApiResponseParser<VKWallGetByIdResponse> {
-            override fun parse(response: String): VKWallGetByIdResponse {
-                try {
-                    val joResponse = JSONObject(response).getJSONObject("response")
-                    val gson = GsonBuilder().setPrettyPrinting().create()
-
-                    return gson.fromJson(joResponse.toString(), VKWallGetByIdResponse::class.java)
-
-                } catch (ex: JSONException) {
-                    throw VKApiIllegalResponseException(ex)
-                }
-            }
-        }
+    class WallGet(parameters: VKParameters) : VKBaseApiCommand<VKWallGetResponse>(
+        parameters = parameters,
+        method = WALL_GET
+    ) {
+        override val classToken = VKWallGetResponse::class.java
     }
 
-    class VideoSave(private val parameters: VKParameters) : ApiCommand<VKVideoSaveResponse>() {
-        override fun onExecute(manager: VKApiManager): VKVideoSaveResponse {
-            val call = VKMethodCall.Builder()
-                .method("video.save")
-                .args(parameters.args)
-                .version(API_VERSION)
-                .build()
-
-            return manager.execute(call, ResponseApiParser())
-        }
-
-        private class ResponseApiParser : VKApiResponseParser<VKVideoSaveResponse> {
-            override fun parse(response: String): VKVideoSaveResponse {
-                try {
-                    val joResponse = JSONObject(response).getJSONObject("response")
-                    val gson = GsonBuilder().setPrettyPrinting().create()
-
-                    return gson.fromJson(joResponse.toString(), VKVideoSaveResponse::class.java)
-
-                } catch (ex: JSONException) {
-                    throw VKApiIllegalResponseException(ex)
-                }
-            }
-        }
+    class WallGetById(parameters: VKParameters) : VKBaseApiCommand<VKWallGetByIdResponse>(
+        parameters = parameters,
+        method = WALL_GET_BY_ID
+    ) {
+        override val classToken = VKWallGetByIdResponse::class.java
     }
 
-    companion object {
-        private const val API_VERSION = "5.101"
+    class VideoSave(parameters: VKParameters) : VKBaseApiCommand<VKVideoSaveResponse>(
+        parameters = parameters,
+        method = VIDEO_SAVE
+    ){
+        override val classToken = VKVideoSaveResponse::class.java
     }
 
 }
---
+++ app/src/main/java/ru/holofox/anicoubs/data/network/api/VKBaseApiCommand.kt
@@ -0,0 +1,49 @@
+package ru.holofox.anicoubs.data.network.api
+
+import com.google.gson.GsonBuilder
+import com.vk.api.sdk.VKApiManager
+import com.vk.api.sdk.VKApiResponseParser
+import com.vk.api.sdk.VKMethodCall
+import com.vk.api.sdk.exceptions.VKApiIllegalResponseException
+import com.vk.api.sdk.internal.ApiCommand
+
+import org.json.JSONException
+import org.json.JSONObject
+
+import ru.holofox.anicoubs.data.db.entity.vk.builder.VKParameters
+
+abstract class VKBaseApiCommand<T: Any>(
+    private val parameters: VKParameters,
+    private val method: String
+) : ApiCommand<T>() {
+
+    protected abstract val classToken: Class<T>
+
+    companion object {
+        private const val API_VERSION = "5.101"
+    }
+
+    override fun onExecute(manager: VKApiManager): T {
+        val call = VKMethodCall.Builder()
+            .method(method)
+            .args(parameters.args)
+            .version(API_VERSION) // or default - manager.config.version
+            .build()
+
+        return manager.execute(call, ResponseApiParser(classToken))
+    }
+
+    protected class ResponseApiParser<T: Any>(val type: Class<T>): VKApiResponseParser<T> {
+        override fun parse(response: String): T {
+            try {
+                val joResponse = JSONObject(response).getJSONObject("response").toString()
+                val gson = GsonBuilder().setPrettyPrinting().create()
+
+                return gson.fromJson(joResponse, type)
+
+            } catch (ex: JSONException) {
+                throw VKApiIllegalResponseException(ex)
+            }
+        }
+    }
+}
